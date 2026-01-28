7303e5720db132d960087cb1807b859957ca44a9
MuhammadWaqasRazaq <mwll9425@gmail.com>
2026-01-28 16:45:16 +0500

    chnages in greeting

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send( '<h1>Hi from Muhammad waqas Secure Pipeline<h1><h2>Cloud Computing<h2> '));
+app.get('/', (req, res) => res.send( '<h1>Assalam o alaikum from Muhammad waqas Secure Pipeline<h1><h2>Cloud Computing<h2> '));
 app.listen(3000, () => console.log('Running on port 3000'));

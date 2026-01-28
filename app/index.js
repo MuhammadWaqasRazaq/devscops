27159459254c33203d65c0947e51be2891a183f3
MuhammadWaqasRazaq <mwll9425@gmail.com>
2026-01-28 14:14:34 +0500

    change heading

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send( '<h1>Hello from waqas Secure Pipeline<h1> <h2> DevSecOps Workshop Working!<h2>   '));
+app.get('/', (req, res) => res.send( '<h1>Hello from waqas Secure Pipeline<h1> '));
 app.listen(3000, () => console.log('Running on port 3000'));
